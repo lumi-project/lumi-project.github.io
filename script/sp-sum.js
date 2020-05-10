7b13313b1a8f08e391a96b3daf9508d425848ec8
lumi-project <marcus.alevarn@gmail.com>
2020-05-10 02:17:29 +0200

    update

--- script/sp-sum.js
+++ script/sp-sum.js
@@ -2,6 +2,7 @@ const sprintBacklogs = document.querySelectorAll('.sprint-backlog');
     
 sprintBacklogs.forEach(sb => {
     let sum = 0;
-    sb.querySelectorAll('.story-point').forEach(sp => sum += parseInt(sp.textContent));
-    sb.parentElement.parentElement.querySelector('.story-points-sum').textContent = sum;
+    sb.querySelectorAll('.story-point span').forEach(sp => sum += parseInt(sp.textContent));
+    console.log(sum);
+    sb.parentElement.parentElement.querySelector('.story-points-sum span').textContent = sum;
 });
