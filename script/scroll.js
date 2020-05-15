1e4081d0eabaa02e9dbbfb1441acc99509a66384
Marcus Alevärn <marcus.alevarn@gmail.com>
2020-05-15 08:25:14 +0200

    added template for expo page

--- script/scroll.js
+++ script/scroll.js
@@ -1,12 +1,20 @@
-const navLinks = document.querySelectorAll('.nav-link');
-const sections = document.querySelectorAll('.section');
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll(".section");
 
-navLinks.forEach((navLink, index) => navLink.addEventListener('click', e => {
-    e.preventDefault();
-    sections[index].scrollIntoView(true);
-}));
+navLinks.forEach((navLink, index) =>
+  navLink.addEventListener("click", (e) => {
+    if (index !== navLinks.length - 1) {
+      e.preventDefault();
+      sections[index].scrollIntoView(true);
+    }
+  })
+);
 
-sections.forEach((section, index) => section.querySelector('.section-title-h1 span').addEventListener('click', e => {
-    e.preventDefault();
-    sections[index].scrollIntoView(true);
-}));
+sections.forEach((section, index) =>
+  section
+    .querySelector(".section-title-h1 span")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      sections[index].scrollIntoView(true);
+    })
+);
--- script/sp-sum.js
+++ script/sp-sum.js
@@ -3,6 +3,5 @@ const sprintBacklogs = document.querySelectorAll('.sprint-backlog');
 sprintBacklogs.forEach(sb => {
     let sum = 0;
     sb.querySelectorAll('.story-point span').forEach(sp => sum += parseInt(sp.textContent));
-    console.log(sum);
     sb.parentElement.parentElement.querySelector('.story-points-sum span').textContent = sum;
 });
